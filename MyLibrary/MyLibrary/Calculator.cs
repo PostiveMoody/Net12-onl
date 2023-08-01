5be2054b67c62f93b9cc883c39ef3824e374c2ad
PostiveMoody <decidedtosolvethisproblem@gmail.com>
2023-08-01 18:45:37 +0300

    Добавил библиотеку чтобы все работало

---
+++ MyLibrary/MyLibrary/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace MyLibrary
+{
+    public static class Calculator
+    {
+        public static double Add(double firstValue, double secondValue) => firstValue + secondValue;
+        public static double Substract(double firstValue, double secondValue) => firstValue - secondValue;
+        public static double Multiply(double firstValue, double secondValue) => firstValue * secondValue;
+        public static double Divide(double firstValue, double secondValue) 
+        {
+            if (secondValue == 0)
+                return 0;
+
+            return firstValue / secondValue;
+        } 
+    }
+}
